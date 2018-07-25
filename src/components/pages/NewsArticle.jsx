911bd8be19d778178f0f3422d33ad610c5ea14c5
unknown <laujh@hawaii.edu>
2018-07-25 11:37:46 -1000

    News Postings

--- src/components/pages/NewsArticle.jsx
+++ src/components/pages/NewsArticle.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Grid, Image } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import { defaultNews } from '../../constants/newsArticles';
 
@@ -12,16 +12,42 @@ class NewsArticle extends React.Component {
     return (
       <div className="kst-landing-background">
         <Container>
-          <section class="mainboxHome">
-            <section class="content">
-              <section class="newsarticle">
+          <section className="mainboxHome">
+            <section className="content">
+              <section className="newsarticle">
                 <h1>
                   {article.title}
                 </h1>
-                {article.description.map((description, index) =>
-                  <p key={index} description={description}>{description[0]}</p>)}
+                {article.description.map((description, index) => {
+                  if (description[1] == "imageLeft") {
+                    return (
+                      <Grid>
+                        <Grid.Column width={3}>
+                          <Image src={article.images[description[2]]} />
+                        </Grid.Column>
+                        <Grid.Column width={13}>
+                          <p key={index} description={description}>{description[0]}</p>
+                          <br></br>
+                        </Grid.Column>
+                      </Grid>
+                    )
+                  }
+                  else if (description[1] == "imageCenter") {
+                    return (
+                      <Grid centered>
+                        <Grid.Column width={4}>
+                          <Image src={article.images[description[2]]} />
+                        </Grid.Column>
+                      </Grid>
+                    )
+                  }
+                  else {
+                    return (<p key={index} description={description}>{description[0]}</p>)
+                  }
+                }
+                )}
               </section>
-              <img src="/images/mountain3.jpg" alt="mountain" class="bottompic"></img>
+              <img src="/images/mountain3.jpg" alt="mountain" className="bottompic"></img>
             </section>
           </section>
         </Container>
